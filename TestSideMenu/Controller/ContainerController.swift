59dc4c3459ea83b4ce9ee1e15702e276bd82c68f
Elle Ti <linh.htu@gmail.com>
2020-03-26 20:56:13 -0700

    Initial setup

---
+++ TestSideMenu/Controller/ContainerController.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerController.swift
+//  TestSideMenu
+//
+//  Created by Elle Ti on 2020-03-26.
+//  Copyright © 2020 Elle Ti. All rights reserved.
+//
+
+import UIKit
+
+class ContainerController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Handlers
+    
+}
---
+++ TestSideMenu/Controller/HomeController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeController.swift
+//  TestSideMenu
+//
+//  Created by Elle Ti on 2020-03-26.
+//  Copyright © 2020 Elle Ti. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Handlers
+    
+}
